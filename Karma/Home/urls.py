dae8df17c63c1b1186d26430c0b656de1a94afc0
Angelika Jarosz <angelikajarosz@gmail.com>
2015-04-25 13:53:35 -0400

    auth progress

--- Karma/Home/urls.py
+++ Karma/Home/urls.py
@@ -6,4 +6,9 @@ urlpatterns = patterns('',
     # Main Page
     url(r'^$', main_page),
 
+    # Auth urls for login/out, password change/reset
+    url('^', include('django.contrib.auth.urls'))
+
+
+
 )
--- Karma/Home/views.py
+++ Karma/Home/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render, render_to_response
-#import templates
+from django.contrib.auth import authenticate, login, logout
+from Home.models import User
+
 
 def main_page(request):
-    return render(request, 'index.html')
+    return render(request, 'base.html')
+
+def register(request):
+    return render(request, 'registration/register.html')
+
+def main_logged_in(request):
+
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            HttpRedirect
+            #Redirect to a success page.
+        else:
+            # Return a 'disabled account' error message
+            ...
+    else:
+        # Return an 'invalid login' error message.
+        ...
+    return render(request, 'registration/login.html')
+
+def logout_view(request):
+    logout(request)
+    # Redirect to a success page.
+
 
 
 
